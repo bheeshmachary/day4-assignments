be0bd60f8a422ee6e2b6e3ca5f4f149d28042177
bheeshmachary <118670478+bheeshmachary@users.noreply.github.com>
2022-11-27 15:33:21 +0530

    Add files via upload

---
+++ simplejava.java
@@ -0,0 +1,20 @@
+
+public class simplejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int a = 25;
+	        int b = 70;
+
+	        if (a > b) {
+	            System.out.println(a + " is Big");
+	        } else {
+	            System.out.println(b + " is Big");
+	        }
+
+	        int ans = a > b ? a : b;
+	        System.out.println(ans + "is big");//20
+	    }
+	}
+
+
