a5e3f6c608a5699c979f3927d1860cda8dfc5d28
bheeshmachary <118670478+bheeshmachary@users.noreply.github.com>
2022-11-27 15:35:46 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,15 @@
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 1;
+while(i <= 5) {
+	System.out.println(i);
+i++;
+	}
+for(int j = 1;j <= 5;j++) {
+	System.out.println(j);
+}
+	}
+}
